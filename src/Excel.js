c8ea5b46d526407bb090c4ed6bd768d623707d6a
kinjoyuji <kinjoyuji@gmail.com>
2017-09-03 02:43:47 +0900

    Hello Excel Components

---
+++ src/Excel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Excel extends React.Component {
+  render(){
+    return(
+      <h1>HelloExcelComponents</h1>
+    );
+  }
+}
+
+export default Excel;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Excel from './Excel';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <h1>HelloWorld</h1>,
+  <Excel />,
     document.getElementById('root'));
 registerServiceWorker();
